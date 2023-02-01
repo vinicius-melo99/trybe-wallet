e67a9d8e08eb06c1d390620550dc3f9c793c085e
Vinícius Melo <viniciusmelo317@gmail.com>
2023-02-01 14:43:55 -0300

    iniciando o projeto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+class App extends Component {
+  render() {
+    return (
+      <div />
+    );
+  }
 }
 
 export default App;
